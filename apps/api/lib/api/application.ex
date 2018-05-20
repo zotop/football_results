cf6aff6216f836684f2bfab59b34b89aa9ab717c
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-05-20 22:12:04 +0200

    setting up api application

--- apps/api/lib/api/application.ex
+++ apps/api/lib/api/application.ex
@@ -1,4 +1,4 @@
-defmodule Database.Application do
+defmodule Api.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
@@ -9,12 +9,12 @@ defmodule Database.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      FootballResults.Repo
+      Plug.Adapters.Cowboy.child_spec(scheme: :http, plug: Api, options: [port: 4001])
     ]
 
-    Logger.info("Starting Database application")
+    Logger.info("Starting Api application")
 
-    opts = [strategy: :one_for_one, name: Database.Supervisor]
+    opts = [strategy: :one_for_one, name: Api.Supervisor]
     Supervisor.start_link(children, opts)
   end
 end
---
+++ apps/api/mix.exs
@@ -0,0 +1,32 @@
+defmodule Api.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :api,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.6",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger, :cowboy, :plug],
+      mod: {Api.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [{:cowboy, "~> 1.0.0"},
+     {:plug, "~> 1.5.0"},
+    ]
+  end
+end
