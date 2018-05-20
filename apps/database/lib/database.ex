e7a6b4b1465cf467fc4babdfc0a9901e3b6ecea7
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-05-20 13:36:48 +0200

    listing results for a given division and season pair

--- apps/database/lib/database.ex
+++ apps/database/lib/database.ex
@@ -5,4 +5,9 @@ import Ecto.Query, only: [from: 2]
     FootballResults.Repo.all(from(result in Database.Result, distinct: true,
                              select: %{division: result.division, season: result.season}))
   end
+
+  def list_results(division: division, season: season) do
+    FootballResults.Repo.all(from(result in Database.Result,
+                             where: [division: ^division, season: ^season]))    
+  end
 end
--- apps/database/spec/database_spec.exs
+++ apps/database/spec/database_spec.exs
@@ -5,15 +5,15 @@ use ESpec
 
     let division_1_2015_2016_barcelona: %{division: "SP1", season: "201516", home_team: "Barcelona"}
     let division_1_2015_2016_valladolid: %{division: "SP1", season: "201516", home_team: "Valladolid"}
-    let division_1_2016_2017_barcelona: %{division: "SP1", season: "201617", home_team: "Barcelona"}
+    let division_1_2016_2017_barcelona: %{division: "SP1", season: "201617", home_team: "Granada"}
     let results: [division_1_2015_2016_barcelona(),
                   division_1_2015_2016_valladolid(),
                   division_1_2016_2017_barcelona()]
+    let! inserted_results: FootballResults.Repo.insert_all(Database.Result, results())
 
     context "division and season pairs for which there are results" do
 
       it "should return unique division and season pairs" do
-        FootballResults.Repo.insert_all(Database.Result, results())
         division_season_pairs = Database.list_division_and_season_pairs()
         divisions = Enum.map(division_season_pairs, fn division_season_pair -> division_season_pair.division end)
         seasons = Enum.map(division_season_pairs, fn division_season_pair -> division_season_pair.season end)
@@ -21,6 +21,20 @@ use ESpec
         expect(length(division_season_pairs)).to be(2)
         expect(divisions).to contain_exactly(["SP1", "SP1"])
         expect(seasons).to contain_exactly(["201617", "201516"])
+      end
+
+    end
+
+    context "results for a specific division and season pair" do
+
+      it "should return the correct results" do
+        results = Database.list_results(division: "SP1", season: "201516")
+        seasons = Enum.map(results, fn result -> result.season end)
+        home_teams = Enum.map(results, fn result -> result.home_team end)
+
+        expect(length(results)).to be(2)
+        expect(seasons).to contain_exactly(["201516", "201516"])
+        expect(home_teams).to contain_exactly(["Valladolid", "Barcelona"])
       end
 
     end
