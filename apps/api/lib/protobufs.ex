b9b0cc0b749647de5bcf9ff8c812db480b55811c
Joao Guerreiro <jmdguerreiro@gmail.com>
2018-05-25 22:30:44 +0200

    Added documentation to Protobufs module

--- apps/api/lib/protobufs.ex
+++ apps/api/lib/protobufs.ex
@@ -1,3 +1,6 @@
 defmodule Protobufs do
+  @moduledoc """
+  Module for accessing protobuf schema definitions
+  """
   use Protobuf, from: Path.wildcard(Path.expand("./proto/*.proto", __DIR__))
 end
